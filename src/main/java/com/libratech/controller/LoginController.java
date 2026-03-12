ec9ab745701444d28219375fbc4774e315ac8a20
Cgeor <cgeorge0529@gmail.com>
2026-03-11 22:19:41 -0400

    Charles Added Maven, Removed the old barebones files, Added RegisterContoller.java,register.html,UserService.java,User.java,LibraTechApplication, Barebone style.css, Add A Layout of Login elements for group members. ALSO READ THE COMMENTS IN THE DIFFERENT FILES

---
+++ src/main/java/com/libratech/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.libratech.controller;
+
+import com.libratech.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class LoginController {
+
+    /** Just added some imports for this to work with the application you can pretty much almost mirror
+     * RegisterController for this - Charles: for Mariam/Muhammed
+     * Also for the html portion look how the register html is laid out to get and Idea of how it would work
+     *
+     * ALSO little thing THIS TOOK ME SOO MUCH LONGER THAN I THOUGHT BUT IT WORKS for register anyway.
+     */
+}
---
+++ src/main/java/com/libratech/service/UserService.java
@@ -0,0 +1,32 @@
+/** This is to check on the backend where the username and email exists in the database to save the user
+ * that the account is linked to. - Charles ADD THINGS THIS IS BAREBONESSSS ;)
+ *  When doing login you can extend this by making a new function to check if the email and password are correct
+ *  to allow the login to occur - Charles: for Mariam/Muhammed
+ */
+package com.libratech.service;
+
+import com.libratech.model.User;
+import com.libratech.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository repo;
+
+    public UserService(UserRepository repo){
+        this.repo = repo;
+    }
+
+    public  String register(User user){
+        if (repo.existsByUsername(user.getUsername()))
+            return "Username already exists";
+
+        if (repo.existsByEmail(user.getEmail()))
+            return "Email already exists";
+
+        repo.save(user);
+
+        return "success";
+    }
+
+}
